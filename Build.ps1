4e3b3f842bd2ce62ca1b77bcbb31e6dc1e8e77ee
unknown <dalemac.09@googlemail.com>
2022-04-18 01:17:30 +0100

    added taunts, updated readme generation

--- Build.ps1
+++ Build.ps1
@@ -1,17 +1,39 @@
 $modFolder = "C:\Users\Dale\Desktop\Code\membtvtauntpack\"
 
-Remove-Item "$modFolder\resources" -Recurse
+# Clean up the previous build.
+Remove-Item "$modFolder\Compiled" -Recurse
 
+# Loop over the aoe2 languages, copy the sounds into the correct language folder structure.
 foreach($language in [System.IO.File]::ReadLines("$modFolder\Languages.txt"))
 {
-	New-Item -ItemType Directory -Force -Path "$modFolder\compiled\resources\$language\sound\taunt"
-	Copy-Item -Path "$modFolder\Source\Audio" -Destination "$modFolder\compiled\resources\$language\sound\taunt" -Recurse
+	New-Item -ItemType Directory -Force -Path "$modFolder\Compiled\resources\$language\sound\taunt"
+	Copy-Item -Path "$modFolder\Source\Audio\*" -Destination "$modFolder\Compiled\resources\$language\sound\taunt" -Recurse
 }
 
-$compress = @{
+# Compress the resources into a .zip file.
+$compressResources = @{
   Path = "$modFolder\Compiled\resources"
   CompressionLevel = "Fastest"
   DestinationPath = "$modFolder\Compiled\resources.zip"
 }
+Compress-Archive @compressResources
 
-Compress-Archive @compress
+
+#Modify the readme.
+$readme = "C:\Users\Dale\Desktop\Code\membtvtauntpack\readme.md"
+$readmeContent = Get-Content $readme -Raw
+$startText = "# Taunts"
+$endText = "# Credits"
+$startTextIndex = $readmeContent.IndexOf($startText) + $startText.Length + 2
+$endTextIndex = $readmeContent.IndexOf($endText) -1
+$readmeContent.Remove($startTextIndex, ($endTextIndex - $startTextIndex )) | Set-Content $readme
+$readmeContent = Get-Content $readme -Raw
+
+$tauntList = @()
+Get-ChildItem "$modFolder\Source\Audio" -Filter *.wem | 
+Foreach-Object {
+    $tauntList += $_.name.Insert(3, ".").Replace(".wem", "");
+}
+$tauntsJoined = $tauntList -join "`n"
+$tauntsJoined += "`n"
+$readmeContent.Insert($startTextIndex, $tauntsJoined) | Set-Content $readme
--- README.md
+++ README.md
@@ -7,15 +7,33 @@ To request a new taunt, either:
 - Submit a PR with the new audio file added. The sound file must be in the `\Source\Audio\` file and in `.wem` format (Use wwise to convert `.wav` files to the `.wem` format).
 
 # Taunts
-
-106. A brutal boom
+106. Titanic
 107. Cavalry coming
-108. Eh, OK
-109. Run away
-110. That for sure
-111. A MIIIIIIIIILL
-112. Screaming in horror
-113. OMG
-114. Shh, don't say anything
-115. What the real fuck
-116. All the damn crossbows in the hell 
+108. Shh, don't say anything
+109. Eh, okay
+110. Run away
+111. That for sure
+112. A MIIIIIIIIILL
+113. This is not acceptable
+114. This is buggedd
+115. This is riggedd
+116. OMG Turkey
+117. OMG boom
+118. A brutal boom
+119. All the damn crossbows in the hell
+120. What
+121. What the fuck man
+122. What the real fuck
+123. Screaming in horror
+124. It's over
+125. Wololololo
+126. Yes, black forest
+127. Being too repetitive
+
+# Credits
+Thanks to MembTv for his amazing aoe2 casting, support him at: https://twitch.tv/membtv
+
+
+
+
+
